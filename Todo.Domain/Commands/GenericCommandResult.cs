f259c7276d6317a8fbad8897a108bc5b8cd22dc3
Humberto Luksevicius <luks.devt@gmail.com>
2022-02-18 15:54:03 -0300

    implementacao base para os commands

---
+++ Todo.Domain/Commands/GenericCommandResult.cs
@@ -0,0 +1,19 @@
+using Todo.Domain.Commands.Contracts;
+
+namespace Todo.Domain.Commands
+{
+  public class GenericCommandResult : ICommandResult
+  {
+    public GenericCommandResult() { }
+    public GenericCommandResult(bool sucess, string message, object data)
+    {
+      Sucess = sucess;
+      Message = message;
+      Data = data;
+    }
+
+    public bool Sucess { get; private set; }
+    public string Message { get; private set; }
+    public object Data { get; private set; }
+  }
+}
